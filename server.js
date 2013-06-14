c5a8c72e4b707c05f13002b27a535ed5d939fa90
Mairead Buchan <mdb.buchan@gmail.com>
2013-06-14 21:45:11 +0100

    replacing code

--- server.js
+++ server.js
@@ -25,6 +25,33 @@ app.listen(port, function() {
   console.log("New web server Listening on " + port);
 });
 
+function display_keyword_form(req, res) {
+
+  var keyword = "sample"; //default value
+  keyword = req.body['keyword'];
+
+
+  function renderPageAgain(title, image, sorryMsg){
+    console.log("outside", title, image, sorryMsg)
+    req.formvalue = req.body['keyword'];
+    req.itemTitle = title;
+    req.returnedImgUrl = image;
+    req.sorryMsg = sorryMsg;
+    render_form_page(req, res);//this isn't forwarding back to page, wrong res and req
+  }
+
+  renderPageAgain("this", "img", "sorry")  
+
+
+}
+
+function render_form_page(req, res) {
+  res.render('keyword_form.ejs', {
+    layout:    false,
+    req:       req
+  });
+}
+
 function show_page(req, res){
   res.render('default.ejs', {
     layout:    false,
@@ -36,3 +63,4 @@ function show_page(req, res){
 // app.post('/', handle_facebook_request);
 
 app.get('/', show_page);
+app.get('/culturequery', display_keyword_form);
