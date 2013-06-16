93445683482dfd19c9a147f4b43eb0438bc24b3b
Mairead Buchan <mdb.buchan@gmail.com>
2013-06-16 14:21:30 +0100

    commenting out and debugging

--- server.js
+++ server.js
@@ -142,41 +142,42 @@ function handle_facebook_request(req, res) {
 
   // if the user is logged in
   if (req.facebook.token) {
-
-    async.parallel([
-      function(cb) {
-        // query 4 friends and send them to the socket for this socket id
-        req.facebook.get('/me/friends', { limit: 4 }, function(friends) {
-          req.friends = friends;
-          cb();
-        });
-      },
-      function(cb) {
-        // query 16 photos and send them to the socket for this socket id
-        req.facebook.get('/me/photos', { limit: 16 }, function(photos) {
-          req.photos = photos;
-          cb();
-        });
-      },
-      function(cb) {
-        // query 4 likes and send them to the socket for this socket id
-        req.facebook.get('/me/likes', { limit: 4 }, function(likes) {
-          req.likes = likes;
-          cb();
-        });
-      },
-      function(cb) {
-        // use fql to get a list of my friends that are using this app
-        req.facebook.fql('SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1', function(result) {
-          req.friends_using_app = result;
-          cb();
-        });
-      }
-    ], function() {
-      render_page(req, res);
-    });
+    console.log("TOKEN...", req.facebook.token);
+    // async.parallel([
+    //   function(cb) {
+    //     // query 4 friends and send them to the socket for this socket id
+    //     req.facebook.get('/me/friends', { limit: 4 }, function(friends) {
+    //       req.friends = friends;
+    //       cb();
+    //     });
+    //   },
+    //   function(cb) {
+    //     // query 16 photos and send them to the socket for this socket id
+    //     req.facebook.get('/me/photos', { limit: 16 }, function(photos) {
+    //       req.photos = photos;
+    //       cb();
+    //     });
+    //   },
+    //   function(cb) {
+    //     // query 4 likes and send them to the socket for this socket id
+    //     req.facebook.get('/me/likes', { limit: 4 }, function(likes) {
+    //       req.likes = likes;
+    //       cb();
+    //     });
+    //   },
+    //   function(cb) {
+    //     // use fql to get a list of my friends that are using this app
+    //     req.facebook.fql('SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1', function(result) {
+    //       req.friends_using_app = result;
+    //       cb();
+    //     });
+    //   }
+    // ], function() {
+    //   render_page(req, res);
+    // });
 
   } else {
+    //the page renders to begin with, calls the first handshake and then fails. 
     render_page(req, res);
   }
 }
