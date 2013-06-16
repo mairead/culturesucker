a615d7b1133b73475d726dc857a8c79459348c20
Mairead Buchan <mdb.buchan@gmail.com>
2013-06-16 15:07:27 +0100

    replacing sample code

--- server.js
+++ server.js
@@ -156,28 +156,28 @@ function handle_facebook_request(req, res) {
           cb();
         });
       }
-      //,
-    //   function(cb) {
-    //     // query 16 photos and send them to the socket for this socket id
-    //     req.facebook.get('/me/photos', { limit: 16 }, function(photos) {
-    //       req.photos = photos;
-    //       cb();
-    //     });
-    //   },
-    //   function(cb) {
-    //     // query 4 likes and send them to the socket for this socket id
-    //     req.facebook.get('/me/likes', { limit: 4 }, function(likes) {
-    //       req.likes = likes;
-    //       cb();
-    //     });
-    //   },
-    //   function(cb) {
-    //     // use fql to get a list of my friends that are using this app
-    //     req.facebook.fql('SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1', function(result) {
-    //       req.friends_using_app = result;
-    //       cb();
-    //     });
-    //   }
+      ,
+      function(cb) {
+        // query 16 photos and send them to the socket for this socket id
+        req.facebook.get('/me/photos', { limit: 16 }, function(photos) {
+          req.photos = photos;
+          cb();
+        });
+      },
+      function(cb) {
+        // query 4 likes and send them to the socket for this socket id
+        req.facebook.get('/me/likes', { limit: 4 }, function(likes) {
+          req.likes = likes;
+          cb();
+        });
+      },
+      function(cb) {
+        // use fql to get a list of my friends that are using this app
+        req.facebook.fql('SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1', function(result) {
+          req.friends_using_app = result;
+          cb();
+        });
+      }
     ], function() {
       console.log("ASYNC RETURNED")
       render_page(req, res);
