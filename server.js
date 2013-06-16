9db10ba4c82517d4c6658c7b70d7fd81a50bd6b5
Mairead Buchan <mdb.buchan@gmail.com>
2013-06-16 10:41:57 +0100

    reworking request call to return async callback for re-rendering page. please work

--- server.js
+++ server.js
@@ -18,54 +18,8 @@ app.listen(port, function() {
   console.log("New web server Listening on " + port);
 });
 
-function display_keyword_form(req, res) {
-
-  var keyword = "sample"; //default value
-  //keyword = req.body['keyword'];
-
-  var url = 'http://www.culturegrid.org.uk/index/select/?q=' + keyword + '&wt=json&fq=pndsterms.thumbnail:[* TO *]'
-
-  request(url, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var imageUrl = "";
-    var sorryMsg = "";
-    var itemTitle = "";
-    var docs = JSON.parse(body).response.docs;
-    var docsLength = docs.length;
-
-    //for each item in array test for pndsterms.thumbnail
-    for (var i = docsLength - 1; i >= 0; i--) {
-       if(docs[i]['pndsterms.thumbnail']){
-        imageUrl = docs[i]['pndsterms.thumbnail'];
-        itemTitle = docs[i]['dc.title'][0];
-        break;
-       }
-    };
-
-    //do I need to add thiings to req here, is it still valid?   
-    // req.formvalue = req.body['keyword'];
-    // req.itemTitle = title;
-    // req.returnedImgUrl = image;
-    // render_form_page(req, res);
-
-    //renderPageAgain(itemTitle, imageUrl);
-    render_form_page(req, res);
-  }
-})
-
-
-  function renderPageAgain(title, image){
-    //console.log("outside", title, image)
-    // req.formvalue = req.body['keyword'];
-    // req.itemTitle = title;
-    // req.returnedImgUrl = image;
-    // render_form_page(req, res);
-  }
-  //renderPageAgain("this", "this", "this")
-}
-
+//view template rendering
 function render_form_page(req, res) {
-  console.log("I'm render");
   res.render('keyword_form.ejs', {
     layout:    false,
     req:       req
@@ -79,6 +33,50 @@ function show_page(req, res){
   });
 }
 
+
+//controller actions
+function display_keyword_form(req, res) {
+  async.parallel([
+    function(cb) {
+      var keyword = "sample"; //default value
+      keyword = req.body['keyword'];
+      var url = 'http://www.culturegrid.org.uk/index/select/?q=' + keyword + '&wt=json&fq=pndsterms.thumbnail:[* TO *]'
+
+      request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var imageUrl = "";
+          var sorryMsg = "";
+          var itemTitle = "";
+          var docs = JSON.parse(body).response.docs;
+          var docsLength = docs.length;
+
+          //for each item in array test for pndsterms.thumbnail
+          for (var i = docsLength - 1; i >= 0; i--) {
+            if(docs[i]['pndsterms.thumbnail']){
+              imageUrl = docs[i]['pndsterms.thumbnail'];
+              itemTitle = docs[i]['dc.title'][0];
+              break;
+            };
+          };
+        };
+        //pack return values into an object
+        var returnObj = {
+          'title':itemTitle,
+          'imageUrl': imageUrl
+        }
+        cb(keyword, returnObj);
+      });//end async request call to GET
+    }
+  ], function(keyword, returnObj){
+    var params = returnObj[0]//async returns params back inside an array object
+    req.formvalue = keyword
+    req.itemTitle = params.title;
+    req.returnedImgUrl = params.imageUrl;
+    render_form_page(req, res);
+  });    
+};
+
+//routing
 app.get('/', show_page);
 app.get('/culturequery', display_keyword_form);
 app.post('/culturequery', display_keyword_form);
